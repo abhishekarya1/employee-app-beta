c40239887bc354987c38eaf75df9e09c07a99a74
abhishekarya1 <37147545+abhishekarya1@users.noreply.github.com>
2020-11-20 15:09:13 +0530

    updated backend

--- mainApp/app.py
+++ mainApp/app.py
@@ -1,14 +1,19 @@
 from flask import *
 import sqlite3
   
-app = Flask(__name__) #creating the Flask class object   
+app = Flask(__name__)  
+app.secret_key = 'mySecretKey#123'
  
-#my additions
-
 @app.route('/')  
-def index():  
+def index():
     return render_template("index.html"); 
 
+@app.route('/home')  
+def home():
+	name = session['name']
+	return render_template("home.html", name = name) 
+
+''' Long and Redundant Way
 @app.route('/accessVerify')  
 def accessVerify():
 		return render_template("accessVerify.html")
@@ -16,15 +21,31 @@ def accessVerify():
 @app.route('/keyVerify', methods = ["POST","GET"])  
 def keyVerify():
 	accessKey = request.form["key"]
-	if accessKey == '345sdf':
+	if accessKey == 'aaa':
 		return render_template("admin.html");
 	else: return "Invalid Access Key!"
+	
 @app.route('/admin')  
 def admin():
-	if request.referrer == '/index/accessVerify':
+	if request.referrer == (request.url_root + 'view'):
 		return render_template("admin.html");
 	else: return "Not Permitted" 
     	
+'''
+@app.route('/accessVerify')  
+def accessVerify():
+		return render_template("accessVerify.html")
+
+@app.route('/admin', methods = ["POST","GET"])  
+def admin():
+	if request.referrer == (request.url_root + 'accessVerify'):
+		accessKey = request.form["key"]
+		if accessKey == 'aq123':
+			return render_template("admin.html")  
+		else: return abort(401)	#invalid access key
+	elif request.referrer == (request.url_root + 'view') or request.referrer == (request.url_root + 'deleteEmployee'):
+		return render_template("admin.html");
+	else: return abort(403)	#direct access 
 
 @app.route('/registerEmployee')  
 def registerEmployee():  
@@ -53,13 +74,14 @@ def result():
             con.close()      
 
 @app.route("/view")
-def view():  
-    con = sqlite3.connect("employee.db")  
-    con.row_factory = sqlite3.Row  
-    cur = con.cursor()  
-    cur.execute("SELECT * FROM Employees")
-    rows = cur.fetchall()
-    return render_template("view.html", rows = rows)
+def view():
+	if request.referrer != (request.url_root + 'admin'): return "Not Permitted"
+	con = sqlite3.connect("employee.db")
+	con.row_factory = sqlite3.Row
+	cur = con.cursor()
+	cur.execute("SELECT * FROM Employees")
+	rows = cur.fetchall()
+	return render_template("view.html", rows = rows)
 
 @app.route('/searchEmployee')  
 def searchEmployee():  
@@ -95,8 +117,46 @@ def searchByFirstLetter():
 	else: msg = "All records matching the search criteria are: "
 	return render_template("search_result.html", msg = msg, rows = rows)
 
+@app.route("/deleteEmployee")
+def deleteEmployee():
+	if request.referrer != (request.url_root + 'admin'): return "Not Permitted"
+	return render_template("deleteEmployee.html")
 	
+@app.route("/delete", methods = ["POST","GET"])
+def delete():
+	msg = "Looks like the database is not connected :("  
+	if request.method == "POST":
+		try:  
+			eid = request.form["eid"]
+			name = request.form["name"]
+			with sqlite3.connect("employee.db") as con:			
+				cur = con.cursor()  
+				cur.execute("SELECT empID FROM employees WHERE empID = (?)", (eid,))
+				rows=cur.fetchall()
+				if len(rows) == 0:	msg="No such record exists."
+				else:
+					cur = con.cursor()  
+					cur.execute("DELETE FROM employees WHERE empID = (?) AND empName = (?)", (eid, name))  
+					con.commit()
+					msg = "Employee record successfully deleted."
+		except:  
+			con.rollback()  
+			msg = "Unable to delete employee record."
+		finally:  
+			return render_template("success.html", msg = msg)
+			con.close() 	
 
+@app.route('/login', methods = ['POST'])
+def login():
+	name = request.form['name']
+	session['name'] = name
+	return redirect(url_for('home'))
+
+@app.route('/logout', methods = ['POST'])
+def logout():
+	if session.pop('name') is not None:
+		return '''<p>Logged out successfully.<p><br><br><a href="/">Login again</a>'''
+	else: return "There was an error in processing your request."	
 #main
 if __name__ =='__main__':  
     app.run(debug = True)  
